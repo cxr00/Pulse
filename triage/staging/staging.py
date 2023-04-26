6a1ad6a30aa244b10b19f2707c4ececc0b307660
Joshua Conrad <jozbornn@gmail.com>
2023-04-26 16:54:52 -0700

    Added comments

--- triage/staging/staging.py
+++ triage/staging/staging.py
@@ -2,22 +2,45 @@ import abc
 
 
 class Staging(abc.ABC):
+    """
+    The base class for staging, which is the backend
+    process which a prompt goes through before it reaches the model
+
+    Create a subclass of this to implement a staging strategy
+    """
 
     def __init__(self):
         pass
 
     @abc.abstractmethod
     def gating(self, prompt):
+        """
+        The process of selectively allowing or blocking certain
+        types of input based on their perceived level of risk potential
+        for adversarial attacks.
+        """
         return prompt
 
     @abc.abstractmethod
     def annotation_verification(self, prompt, lr, log=False):
+        """
+        The process through which a prompt's annotations are verified
+        to be accurate.
+        """
         return prompt
 
     @abc.abstractmethod
     def layering(self, prompt):
+        """
+        The act of subjecting a prompt to a series of meta-prompts
+        to refine or sanitise it.
+        """
         return prompt
 
     @abc.abstractmethod
     def vaccination(self, prompt):
+        """
+        The act of inserting default annotations and meta-prompts
+        into a prompt to make it more reslient to attack.
+        """
         return prompt
