bf0e62af9792575e0f50f598246167628cb2d65f
Joshua Conrad <jozbornn@gmail.com>
2023-04-29 11:26:25 -0700

    Added info on api rates and default parameters

---
+++ triage/staging/_api_rates.py
@@ -0,0 +1,20 @@
+
+per = 1000
+
+gpt_35_turbo = {
+    "name": "gpt-3.5-turbo",
+    "prompt": .002,
+    "completion": .002
+}
+
+gpt_4_8k = {
+    "name": "gpt-4",
+    "prompt": .03,
+    "completion": .06
+}
+
+gpt_4_32k = {
+    "name": "gpt-4-32k",
+    "prompt": .06,
+    "completion": .12
+}
---
+++ triage/staging/_params.py
@@ -0,0 +1,17 @@
+
+default_params = {
+    "model": "gpt_35_turbo",
+    "suffix": "[/]",
+    "max_tokens":  1024,
+    "temperature": 0.7,
+    "top_p": 1,
+    "n": 1,
+    "stream": False,
+    "logprobs": None,
+    "echo": False,
+    "stop": None,
+    "presence_penalty": 0,
+    "frequency_penalty": 0,
+    "best_of": 1,
+    "logit_bias": {}
+}
