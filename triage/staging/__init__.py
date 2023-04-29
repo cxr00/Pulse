4b2fc3ed5b7523340171bab060edd9d48cecbc1a
Joshua Conrad <jozbornn@gmail.com>
2023-04-29 11:26:42 -0700

    Import defaults before staging

--- triage/staging/__init__.py
+++ triage/staging/__init__.py
@@ -1,2 +1,6 @@
+from triage.staging._api_rates import gpt_35_turbo, gpt_4_8k, gpt_4_32k, per
+from triage.staging._blocklist import blocklist
+from triage.staging._params import default_params
+
 from triage.staging.staging import Staging
 from triage.staging.basic_staging import BasicStaging
