ef2194257dc195dd9bf2ee5e9d1ece46acf2c3bf
Joshua Conrad <jozbornn@gmail.com>
2023-05-02 16:36:30 -0700

    Remove unused URLs

--- web/__init__.py
+++ web/__init__.py
@@ -2,6 +2,3 @@ from web.flask_api import app, host, port
 
 pulse_api_url = f"http://{host}:{port}/prompts"
 pulse_user_api_url = f"{pulse_api_url}/users/"
-pulse_risk_api_url = f"{pulse_api_url}/risk/"
-pulse_overhead_api_url = f"{pulse_api_url}/overhead/"
-pulse_model_params_api_url = f"{pulse_api_url}/params/"
