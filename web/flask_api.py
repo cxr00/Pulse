3e63a3fa503a1f7af7e89eb37629c67784b27924
Joshua Conrad <jozbornn@gmail.com>
2023-05-02 12:10:22 -0700

    Store prompts rather than just their triage

--- web/flask_api.py
+++ web/flask_api.py
@@ -8,50 +8,46 @@ local_dir = "local"
 save = True
 
 # Data storage
-prompts = {prompt["prompt_id"]: prompt.triage for prompt in Prompt.load_folder(local_dir)}
+prompts = {prompt["prompt_id"]: prompt for prompt in Prompt.load_folder(local_dir)}
 CURRENT_PROMPT_ID = max([-1] + [int(prompt["prompt_id"]) for prompt in prompts.values()]) + 1
 
 # GET endpoints
 @app.route('/prompts')
 def get_prompts():
-    return jsonify(list(prompts.values()))
+    return jsonify([prompt.triage for prompt in prompts.values()])
 
 @app.route('/prompts/<prompt_id>')
 def get_prompt(prompt_id):
     prompt = prompts.get(prompt_id)
     if prompt:
-        prompt = {key: value for key, value in prompt.items() if key in request.args}
+        prompt = {key: value for key, value in prompt.triage.items() if key in request.args}
         return jsonify(prompt)
     else:
         return jsonify({'error': 'Prompt not found'}), 404
 
 @app.route('/prompts/users/<user_id>')
 def get_users_prompts(user_id):
-    users = list(set([prompt["u_id"] for prompt_id, prompt in prompts.items()]))
+    users = list(set([prompt.triage["u_id"] for prompt_id, prompt in prompts.items()]))
     if user_id == "all":
-        return jsonify(list(prompts.values()))
+        return jsonify([prompt.triage for prompt in prompts.values()])
     elif user_id in users:
-        output_prompts = [prompt for prompt in prompts.values() if prompt["u_id"] == user_id]
+        output_prompts = [prompt.triage for prompt in prompts.values() if prompt["u_id"] == user_id]
         return jsonify(output_prompts)
     else:
         return jsonify({"error": "User id not found"}), 404
-
-@app.route('/prompts/risk/user/<u_id>')
-def get_triage_summary(u_id):
-    return jsonify({'triage_summary': 'Summary for user ' + u_id})
 
 @app.route('/prompts/overhead/<prompt_id>')
 def get_overheads(prompt_id):
     prompt = prompts.get(prompt_id)
     if prompt:
-        prompt = {key: value for key, value in prompt.items() if "overhead" in key}
+        prompt = {key: value for key, value in prompt.triage.items() if "overhead" in key}
         return jsonify(prompt)
     else:
         return jsonify({'error': 'Prompt not found'}), 404
 
 @app.route('/prompts/params/<prompt_id>')
 def get_params(prompt_id):
-    prompt = prompts.get(prompt_id)["model_parameters"]
+    prompt = prompts.get(prompt_id).triage["model_parameters"]
     if prompt:
         return jsonify(prompt)
     else:
@@ -67,7 +63,7 @@ def stage_prompt():
     prompt_data['prompt_id'] = prompt_id
     to_add = Prompt(**prompt_data)
     to_add.stage(save=save)
-    prompts[prompt_id] = to_add.triage
+    prompts[prompt_id] = to_add
     return jsonify(to_add.triage)
 
 
@@ -75,6 +71,7 @@ def stage_prompt():
 @app.route('/prompts/<prompt_id>', methods=['DELETE'])
 def delete_prompt(prompt_id):
     if prompt_id in prompts:
+        prompts[prompt_id].delete()
         del prompts[prompt_id]
         return jsonify({'success': True})
     else:
