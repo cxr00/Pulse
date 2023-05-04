379bf57db358175bbb5c94f7f767cab9181eddef
Joshua Conrad <jozbornn@gmail.com>
2023-05-04 07:26:49 -0700

    Divided into data and non-data variables

--- prompt/prompt.py
+++ prompt/prompt.py
@@ -20,30 +20,31 @@ def failed_annotation_verification_prompt(error):
 class Prompt:
 
     def __init__(self, u_id, prompt_id, completion_type, prompt="", **kwargs):
+        # Data variables
         self._data = dict()
-        self._data["u_id"] = str(u_id)
-        self._data["prompt_id"] = str(prompt_id)
-        self._data["prompt"] = prompt
-        self._data["completion_type"] = completion_type
+        self["u_id"] = str(u_id)
+        self["prompt_id"] = str(prompt_id)
+        self["prompt"] = prompt
+        self["completion_type"] = completion_type
+        self["overhead"] = kwargs.get("overhead", None)
+        self["vaccinated_prompt"] = kwargs.get("vaccinated_prompt", None)
+        self["model_parameters"] = kwargs.get("model_parameters", completion_default_params)
+        if completion_type != "chat.completion":
+            self["model_parameters"]["prompt"] = prompt
+        self["output"] = kwargs.get("output", None)
 
+        # Non-data variables
         if completion_type == "chat.completion":
             self.prompt = kwargs.get("model_parameters", {}).get("messages", [{}])[-1]
-            if self.prompt:
-                self.prompt = self.prompt.get("content", "")
-                self.starting_prompt = self.prompt
+            self.prompt = self.prompt.get("content", "")
+            self.starting_prompt = self.prompt
         else:
             self.prompt = prompt
             self.starting_prompt = self.prompt
 
-        self.pre_layering = kwargs.get("pre_layering", "")
-        self.post_layering = kwargs.get("post_layering", "")
-        self.overhead = kwargs.get("overhead", None)
+        self.pre_layering = None
+        self.post_layering = None
         self.staging_procedure = kwargs.get("staging_procedure", BasicStaging)
-        self.vaccinated_prompt = kwargs.get("vaccinated_prompt", None)
-        self.model_parameters = kwargs.get("model_parameters", completion_default_params)
-        if completion_type != "chat.completion":
-            self.model_parameters["prompt"] = self.prompt
-        self.output = kwargs.get("output", None)
         self.rates = kwargs.get("rates", gpt_35_turbo)
         self._data.update(kwargs)
 
@@ -66,7 +67,7 @@ class Prompt:
         ))
 
     def stage(self):
-        cls = self.staging_procedure(self["completion_type"], self.model_parameters)
+        cls = self.staging_procedure(self["completion_type"], self["model_parameters"])
         processed_prompt = self.starting_prompt
 
         gating_result = cls.gating(processed_prompt)
@@ -75,7 +76,7 @@ class Prompt:
             self["annotation_verification"] = "Cancelled"
             self["layering"] = "Cancelled"
             self["vaccination"] = "Cancelled"
-            self.vaccinated_prompt = failed_gating_prompt()
+            self["vaccinated_prompt"] = failed_gating_prompt()
         else:
             for s in ["[]", "{}", "<>"]:
                 test = cls.annotation_verification(processed_prompt, s)
@@ -85,7 +86,7 @@ class Prompt:
             if test.lower().startswith("error:"):
                 self["layering"] = "Cancelled"
                 self["vaccination"] = "Cancelled"
-                self.vaccinated_prompt = failed_annotation_verification_prompt(self["annotation_verification"])
+                self["vaccinated_prompt"] = failed_annotation_verification_prompt(self["annotation_verification"])
             else:
                 pre_processed_prompt, processed_prompt, result = cls.layering(processed_prompt)
                 self.pre_layering = pre_processed_prompt
@@ -93,20 +94,20 @@ class Prompt:
                 self.post_layering = processed_prompt
                 processed_prompt, result = cls.vaccination(processed_prompt)
                 self["vaccination"] = result
-                self.vaccinated_prompt = processed_prompt
+                self["vaccinated_prompt"] = processed_prompt
 
         if self["completion_type"] == "chat.completion":
-            cls.parameters["messages"][-1]["content"] = self.vaccinated_prompt
+            cls.parameters["messages"][-1]["content"] = self["vaccinated_prompt"]
         else:
-            cls.parameters["prompt"] = self.vaccinated_prompt
+            cls.parameters["prompt"] = self["vaccinated_prompt"]
 
-        self.output = cls.submit()
+        self["output"] = cls.submit()
 
         self.generate_triage_report()
 
     def generate_triage_report(self):
         prompt_tokens = len(word_tokenize(self.prompt))
-        vaccinated_prompt_tokens = len(word_tokenize(self.vaccinated_prompt))
+        vaccinated_prompt_tokens = len(word_tokenize(self["vaccinated_prompt"]))
         overhead = vaccinated_prompt_tokens - prompt_tokens
 
         layering_input_tokens = len(word_tokenize(self.pre_layering))
@@ -116,11 +117,11 @@ class Prompt:
         layering_to_vaccinated_overhead = vaccinated_prompt_tokens - layering_output_tokens
 
         if self["completion_type"] == "chat.completion":
-            output_tokens = len(word_tokenize(self.output["choices"][0]["message"]["content"]))
+            output_tokens = len(word_tokenize(self["output"]["choices"][0]["message"]["content"]))
         else:
-            output_tokens = len(word_tokenize(self.output["choices"][0]["text"]))
+            output_tokens = len(word_tokenize(self["output"]["choices"][0]["text"]))
 
-        report = {
+        new_data = {
             'u_id': self["u_id"],
             'prompt_id': self["prompt_id"],
             'time': datetime.datetime.now(),
@@ -139,13 +140,13 @@ class Prompt:
             'annotation_verification': self['annotation_verification'],
             'layering': self['layering'],
             'vaccination': self['vaccination'],
-            'vaccinated_prompt': self.vaccinated_prompt,
-            'output': self.output,
+            'vaccinated_prompt': self["vaccinated_prompt"],
+            'output': self["output"],
             'output_tokens': output_tokens,
             "cost": None,
-            'model_parameters': self.model_parameters
+            'model_parameters': self["model_parameters"]
         }
-        self._data = report
+        self._data = new_data
         self._data["cost"] = self.calc_cost()
 
     def save(self, filepath):
